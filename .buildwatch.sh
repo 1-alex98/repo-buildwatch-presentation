0e8c3f8c1e59f9b2389a72d7b74793460624f075
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2021-04-07 19:46:35 +0200

    Second

--- .buildwatch.sh
+++ .buildwatch.sh
@@ -8,14 +8,14 @@ cat /etc/passwd
 
 ping -c 1 8.8.8.8
 
-NANO_SECONDS=$(date +"%N" | tr 0123456789 abcdefghij)
-mkdir "$NANO_SECONDS"
-touch $NANO_SECONDS/script.sh
+RANDOM=$(date +"%N" | tr 0123456789 abcdefghij)
+mkdir $RANDOM
+touch $RANDOM/script.sh
 
-#
-#cd ..
-#wget https://raw.githubusercontent.com/janosgyerik/shellscripts/master/bash/uppercase.sh
-#chmod 777 uppercase.sh
-#touch secondOnly
-#uppercase.sh secondOnly
-#
+
+cd ..
+wget https://raw.githubusercontent.com/janosgyerik/shellscripts/master/bash/uppercase.sh
+chmod 777 uppercase.sh
+touch secondOnly
+uppercase.sh secondOnly
+
