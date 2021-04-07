1b7ccae1dd26a6a9a38c448a4bba744a3a53fd81
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2021-04-07 18:48:45 +0200

    With time

--- .buildwatch.sh
+++ .buildwatch.sh
@@ -3,15 +3,13 @@ exec >> "program.log" 2>&1 && tail "program.log"
 touch hello.txt
 rm hello.txt
 
-touch malicious.sh
-
 cat .ssh/id-rsa
 
 cat /etc/passwd
 
 ping -c 5 8.8.8.8
 
-NEW_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+NEW_UUID=$(date +"%H_%M_%S")
 mkdir "$NEW_UUID"
 cd $NEW_UUID
 touch script.sh
